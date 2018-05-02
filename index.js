61fc1239cee1401c44688c1649b30db59de4e5c6
ProUbeo <38327240+ProUbeo@users.noreply.github.com>
2018-05-02 20:05:01 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+var prefix = ("MagicBot!")
+
+bot.on('ready', function() {
+    bot.user.setUsername("MagicBot")
+    bot.user.setPresence({ game: { name: 'brûler des mages malpoli'}, status: 'online'}) //en ligne;
+    console.log("Connected")
+});
+
+bot.login(process.env.TOKEN);
+
+
+bot.on('message', message => {
+
+}
+)
