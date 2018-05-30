e3d4e5cf30c6eb098eba066bba9059017b5d5e89
ProUbeo <38327240+ProUbeo@users.noreply.github.com>
2018-05-30 18:16:50 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+const client = new Discord.Client();
+
+const adapter = new FileSync('database.json');
+const db = low(adapter);
+
+db.defaults({ histoires: [],  xp: []}).write()
 
 var prefix = ("M!")
 
@@ -13,6 +21,32 @@ bot.login(process.env.TOKEN);
 
 
 bot.on('message', message => {
+    var msgauthor = message.author.id;
+
+    if(message.author.bot)return;
+
+if(!db.get("xp").find({user : msgauthor}).value()){
+    db.get("xp").push({user: msgauthor, xp: 1}).write();
+    }else{
+        var userxpdb = db.get("xp").filter({user: msgauthor}).find('xp').value();
+        console.log(userxpdb);
+        var userxp = Object.values(userxpdb)
+        console.log(userxp)
+        console.log(`nombre d'xp; ${userxp[1]}`)
+        
+        db.get("xp").find({user: msgauthor}).assign({user: msgauthor, xp: userxp[1] += 1}).write();
+
+        if(message.content === "M!xp"){
+            var xp = db.get("xp").filter({user: msgauthor}).find('xp').value()
+            var xpfinal = Object.values(xp);
+            var xp_embed = new Discord.RichEmbed()
+            .setTitle(`XP de ${message.author.username}`)
+            .setColor('0xcc0099')
+            .setDescription("Affichage des XP")
+            .addField("XP:", `${xpfinal[1]} xp`)
+            .setFooter(":D")
+            message.channel.send({embed: xp_embed});
+        }}
     if(message.content.startsWith("m!feu")){
         message.channel.send(`**${message.author.username}** lance la capacité de feu !\nDégâts : 25`,{
         file: "https://orangemushroom.files.wordpress.com/2014/04/ignition-effect.gif"
